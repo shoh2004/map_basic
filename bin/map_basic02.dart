ddea1e40de640a72e147cdc3cfa1d42110daab2d
shohrux <shohruxergashev67@gmail.com>
2022-07-24 10:53:43 +0500

    3280

--- bin/map_basic02.dart
+++ bin/map_basic02.dart
@@ -4,11 +4,12 @@ Create a map called data add items at least two.
 Return the variable.
 
 */
-
 Map func() {
-  return;
+  Map data = {1: 'one', 1: 'two'};
+  data[3] = 3;
+  return data;
 }
 
 void main() {
   print(func());
-}
+} 
